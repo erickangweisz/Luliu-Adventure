364a41897ac28751f373a45829a056b4f0376b97
erickangweisz <erickangweisz@gmail.com>
2016-10-07 18:51:50 +0100

    commit realizado desde netbeans para prueba de plugin en la actividad A2 - PluginGitEnNetbeans

--- src/principal/Constantes.java
+++ src/principal/Constantes.java
@@ -3,7 +3,7 @@ package principal;
 import java.awt.Color;
 import java.awt.Font;
 import principal.herramientas.CargadorRecursos;
-// cambio para ejercicio dpl.
+// cambio para ejercicio dpl A2.
 
 public class Constantes 
 {
